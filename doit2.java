e1e2dd9e1d24b184743d2689f447e3af5fefb6f3
NEWJHJ <wngywls159@naver.com>
2022-11-07 02:43:59 +0900

    plus

--- doit2.java
+++ doit2.java
@@ -6,14 +6,7 @@ public class doit2 {
 //      System.out.print("*");
 //      a++;
 //    }
-//     System.out.println(" ");
-
-//    for (int b = 3; b >= 1 ; b--) {
-//      for (int c = 1; c <= b; c++) {
-//        System.out.print(" ");
-//      }
-//      System.out.println();
-//    }
+//     System.out.println(" "); //한 줄
 
       int i = 3;
     for (int e = 1; e <= 7; e += 2) {
@@ -25,8 +18,27 @@ public class doit2 {
         System.out.print("*");
       }
       System.out.println();
-    } // ???
+    } // 피라미드
 
+
+    for (int e = 1; e <= 4; e += 1) {
+        for (int d = 1; d <= 4 - e; d++) {
+          System.out.print(" ");
+        }
+        for (int f = 1; e * 2 - 1 >= f; f++) {
+          System.out.print("*");
+        }
+     System.out.println();
+    }
+    for (int e = 3; e >= 1; e -= 1) {
+      for (int d = 3; d >= e; d--) {
+        System.out.print(" ");
+      }
+      for (int f = 1; e * 2 - 1 >= f; f++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    } // 마름모
 
 
   }
